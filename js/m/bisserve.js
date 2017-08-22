2d39ae3722e49f944cc3f5bdaba9c89b4bfacd28
zty <1048972989@qq.com>
2017-08-22 11:44:10 +0800

    id方面的

--- js/m/bisserve.js
+++ js/m/bisserve.js
@@ -1,122 +1,165 @@
-$(document).ready(function(){
+$(document).ready(function() {
     /*平安钟服务数量增加减*/
-    $(function() { 
-        $(".addv1").click(function() {  
+    $(function() {
+        $(".addv1").click(function() {
             var t = $(".text_boxv1").text();
-            if(parseInt(t)==""||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv1").text(0);  
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv1").text(0);
             }
-            $(".text_boxv1").text(parseInt(t) + 1)  
+            $(".text_boxv1").text(parseInt(t) + 1)
             setTotalv1();
         })
-        $(".minv1").click(function() {  
-            var t = $(".text_boxv1").text();  
-            if(parseInt(t)==""||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv1").text(0);  
+        $(".minv1").click(function() {
+            var t = $(".text_boxv1").text();
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv1").text(0);
             }
             $(".text_boxv1").text(parseInt(t) - 1)
-            if(parseInt(t)<="0"||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv1").text(0);  
+            if (parseInt(t) <= "0" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv1").text(0);
             }
             setTotalv1();
         })
-        function setTotalv1() {  
+
+        function setTotalv1() {
             var t = $(".text_boxv1").text();
-            if(parseInt(t)==""||undefined||null ||isNaN(t) || isNaN(parseInt(t))){  
-                    t=0;  
-                }  
-            var p = $(".pricev1").val();    
-            var s = parseInt(t) * parseFloat(p);  
-            $(".totalv1").html(s.toFixed(2));  
-        }
-     }); 
-    /*15分钟报告服务数量增加减*/
-     $(function() {    
-        $(".addv2").click(function() {  
-            var t = $(".text_boxv2").text();
-            if(parseInt(t)==""||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv2").text(0);  
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                t = 0;
             }
-            $(".text_boxv2").text(parseInt(t) + 1)  
+            var p = $(".pricev1").val();
+            var s = parseInt(t) * parseFloat(p);
+            $(".totalv1").html(s.toFixed(2));
+        }
+    });
+    /*15分钟报告服务数量增加减*/
+    $(function() {
+        $(".addv2").click(function() {
+            var t = $(".text_boxv2").text();
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv2").text(0);
+            }
+            $(".text_boxv2").text(parseInt(t) + 1)
             setTotalv2();
         })
-        $(".minv2").click(function() {  
-            var t = $(".text_boxv2").text();  
-            if(parseInt(t)==""||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv2").text(0);  
+        $(".minv2").click(function() {
+            var t = $(".text_boxv2").text();
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv2").text(0);
             }
             $(".text_boxv2").text(parseInt(t) - 1)
-            if(parseInt(t)<="0"||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv2").text(0);  
+            if (parseInt(t) <= "0" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv2").text(0);
             }
             setTotalv2();
         })
-        function setTotalv2() {  
+
+        function setTotalv2() {
             var t = $(".text_boxv2").text();
-            if(parseInt(t)==""||undefined||null ||isNaN(t) || isNaN(parseInt(t))){  
-                    t=0;  
-                }  
-            var p = $(".pricev2").val();    
-            var s = parseInt(t) * parseFloat(p);  
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                t = 0;
+            }
+            var p = $(".pricev2").val();
+            var s = parseInt(t) * parseFloat(p);
             $(".totalv2").html(s.toFixed(2));
             $(".text_boxv2_val").text(t);
-        } 
+        }
     });
     /*24小时报告服务数量增加减*/
-     $(function() {    
-        $(".addv3").click(function() {  
+    $(function() {
+        $(".addv3").click(function() {
             var t = $(".text_boxv3").text();
-            if(parseInt(t)==""||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv3").text(0);  
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv3").text(0);
             }
-            $(".text_boxv3").text(parseInt(t) + 1)  
+            $(".text_boxv3").text(parseInt(t) + 1)
             setTotalv3();
         })
-        $(".minv3").click(function() {  
-            var t = $(".text_boxv3").text();  
-            if(parseInt(t)==""||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv3").text(0);  
+        $(".minv3").click(function() {
+            var t = $(".text_boxv3").text();
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv3").text(0);
             }
             $(".text_boxv3").text(parseInt(t) - 1)
-            if(parseInt(t)<="0"||undefined||null||isNaN(t) || isNaN(parseInt(t))){  
-                $(".text_boxv3").text(0);  
+            if (parseInt(t) <= "0" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                $(".text_boxv3").text(0);
             }
             setTotalv3();
         })
-        function setTotalv3() {  
+
+        function setTotalv3() {
             var t = $(".text_boxv3").text();
-            if(parseInt(t)==""||undefined||null ||isNaN(t) || isNaN(parseInt(t))){  
-                    t=0;  
-                }  
-            var p = $(".pricev3").val();    
-            var s = parseInt(t) * parseFloat(p);  
+            if (parseInt(t) == "" || undefined || null || isNaN(t) || isNaN(parseInt(t))) {
+                t = 0;
+            }
+            var p = $(".pricev3").val();
+            var s = parseInt(t) * parseFloat(p);
             $(".totalv3").html(s.toFixed(2));
             $(".text_boxv3_val").text(t);
-        } 
+        }
     });
 
-      /*15和24切换*/
-      $(".15pagon").click(function(){
+    /*15和24切换*/
+    $(".15pagon").click(function() {
         $(".tabsv2").hide();
         $(".tabsv1").show();
-      });
-      $(".24pagon").click(function(){
+    });
+    $(".24pagon").click(function() {
         $(".tabsv1").hide();
         $(".tabsv2").show();
-      });
-      /*返回按钮*/
-      $(".back").click(function(){
-        $(".back").attr("src","../../img/bisserve/backv2.png");
-      });
+    });
+    /*返回按钮*/
+    $(".back").click(function() {
+        $(".back").attr("src", "../../img/bisserve/backv2.png");
+    });
 });
 /* 弹出层*/
-function payshow_show(){
-  $(".payshow").show();
+function payshow_show() {
+    $(".payshow").show();
 }
-$(".payshow").on("click",function(event){
-     event.stopPropagation();
-     var target=event.target;
-     if (!$(target).closest(".modal-content").length>0||$(target).attr("class").indexOf("close-mod")!=-1) {
+$(".payshow").on("click", function(event) {
+    event.stopPropagation();
+    var target = event.target;
+    if (!$(target).closest(".modal-content").length > 0 || $(target).attr("class").indexOf("close-mod") != -1) {
         $(".payshow").hide()
-     };
+    };
 })
+
+/*六次购买服务*/
+$(".buyv1").click(function() {
+    var setid = $("#serviceidv1").val();
+    $("#servicetype").val(setid);
+    $("#servicenumber").val("53");
+    $(".payshow").show();
+});
+$(".buyv2").click(function() {
+    var setid = $("#serviceidv2").val();
+    $("#servicetype").val(setid);
+    $("#servicenumber").val("21");
+    $(".payshow").show();
+});
+$(".buyv3").click(function() {
+    var sernum = $(".text_boxv2").text();
+    var setid = $("#serviceidv3").val();
+    $("#servicetype").val(setid);
+    $("#servicenumber").val(sernum);
+    $(".payshow").show();
+});
+$(".buyv4").click(function() {
+    var setid = $("#serviceidv4").val();
+    $("#servicetype").val(setid);
+    $("#servicenumber").val("53");
+    $(".payshow").show();
+});
+$(".buyv5").click(function() {
+    var setid = $("#serviceidv5").val();
+    $("#servicetype").val(setid);
+    $("#servicenumber").val("21");
+    $(".payshow").show();
+});
+$(".buyv6").click(function() {
+    var sernum = $(".text_boxv3").text();
+    var setid = $("#serviceidv6").val();
+    $("#servicetype").val(setid);
+    $("#servicenumber").val(sernum);
+    $(".payshow").show();
+});
