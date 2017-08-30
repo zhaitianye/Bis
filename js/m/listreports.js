b816bd7f9c2b8c0981ef323e6501b123b439ba44
zty <1048972989@qq.com>
2017-08-30 14:22:18 +0800

    mescroll

--- js/m/listreports.js
+++ js/m/listreports.js
@@ -58,100 +58,96 @@ $(document).ready(function() {
         $(this).addClass("bg-white-sel");
     })
 });
+
 /*滚动部分js*/
-$(document).ready(function() {
 /*滚动部分的高度*/
 var wrapperheight = $("body").height() - $("header").height() - $(".select_munu").height();
-$("#wrapper").css("height", wrapperheight);
-    /*定义滚动区域*/
-    var myScroll = new IScroll("#wrapper", {
-        useTransition: true, //是否使用CSS3的Transition属性，否则使用requestAnimationFram代替
-        useTransform: true, //是否使用CSS3的Transform属性
-        mouseWheel: true, //是否监听鼠标滚轮事件。
-        scrollbars: "custom", //只有该属性的值为"custom"，才能通过.iScrollVerticalScrollbar和.iScrollIndicator设置凹槽和滚动条的样式.(是否显示默认滚动条)
-        interactiveScrollbars: true, //用户是否可以拖动滚动条
-        resizeScrollbars: false, //当这个属性设置为否时,才可以通过.iScrollIndicator改变滚动条(不是凹槽的的宽和高）是否固定滚动条大小，建议自定义滚动条时可开启。  
-        probeType: 3 //这个值设置为3而且必须引入iscroll-probe.js才能触发onscroll事件.需要使用iscroll-probe.js才能生效probeType：1.滚动不繁忙的时候触发2.滚动时每隔一定时间触发3.每滚动一像素触发一次
-    });
+$("#mescroll").css("height", wrapperheight);
+$(document).ready(function() {
+    $(function() {
+        //创建MeScroll对象,内部已默认开启下拉刷新,自动执行up.callback,重置列表数据;
+        var mescroll = new MeScroll("mescroll", {
+            up: {
+                page: { size: 5 }, //每次加载1条数据,模拟loadFull
+                loadFull: {
+                    use: true, //列表数据过少,不足以滑动触发上拉加载,是否自动加载下一页,直到满屏或者无更多数据为止;默认false,因为可通过调高page.size避免这个情况
+                    delay: 500 //延时执行的毫秒数; 延时是为了保证列表数据或占位的图片都已初始化完成,且下拉刷新上拉加载中区域动画已执行完毕;
+                },
+                clearEmptyId: "dataList", //1.下拉刷新时会自动先清空此列表,再加入数据; 2.无任何数据时会在此列表自动提示空
+                callback: getListData, //上拉回调,此处可简写; 相当于 callback: function (page) { getListData(page); }
+            }
+        });
 
-    var list = $("#list");
-    var step = 5; //上拉加载时的动态创建的li的个数
-    var max = $(".listallnumbs").val(); //li的最大个数
-    var end = false; //数据加载完成后的标识
-    var flag = false; //数据到达底部标识
+        /*联网加载列表数据  page = {num:1, size:10}; num:当前页 从1开始, size:每页数据条数 */
+        function getListData(page) {
+            //联网加载数据
+            console.log("page.num==" + page.num);
+            getListDataFromNet(page.num, page.size, function(data) {
+                //联网成功的回调,隐藏下拉刷新和上拉加载的状态;
+                mescroll.endSuccess(data.length); //传参:数据的总数; mescroll会自动判断列表如果无任何数据,则提示空;列表无下一页数据,则提示无更多数据;
+                //设置列表数据,因为配置了emptyClearId,第一页会清空dataList的数据,所以setListData应该写在最后;
+                setListData(data);
+            }, function() {
+                //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
+                mescroll.endErr();
+            });
+        }
 
-    myScroll.on("scroll", function() {
-        var inthis = this;
-        if (parseInt(inthis.y) >= 0 && inthis.directionY == -1) { //当滚动条到上方是或者判断滚动为负值时加载up层数据
-            $("#scroller_up").show(); //up层显示
-            if (parseInt(inthis.y) == 0) { //当滚动条归为时
-                setTimeout(function() { //延迟加载
-                    $("#scroller_up").hide(); //up层隐藏
-                    //inthis.refresh();
-                    location.reload(); //页面重新加载
-                }, 1000)
+        /*设置列表数据*/
+        function setListData(data) {
+            var listDom = document.getElementById("dataList");
+            for (var i = 0; i < data.length; i++) {
+                var pd = data[i];
+
+                var str="";
+                    str += "<li class=\"clear mg-10 radius-10 bg-white pt-20 pb-15\">";
+                    str += "    <div class=\"col-xs-3 pl-20 pr-20\">";
+                    str += "        <img src=\""+ pd.pdImg +"\" class=\"full-w\" alt=\"\">";
+                    str += "    <\/div>";
+                    str += "    <div class=\"col-xs-9 pl-15 pr-0\">";
+                    str += "        <p class=\"f-16 col-5F5F5F f-w pt-10 pb-10\">";
+                    str += "            " + pd.pdName + "";
+                    str += "        <\/p>";
+                    str += "        <p class=\"f-16 col-5F5F5F f-w pt-10 \">";
+                    str += "            "+ pd.pdTime +" <span class=\""+ pd.pdCol +" f-14 pull-right dis-ib mr-20\">"+ pd.pdstate +"<\/span>";
+                    str += "        <\/p>";
+                    str += "    <\/div>";
+                    str += "<\/li>";
+                // var str = '<img class="pd-img" src="' + pd.pdImg + '"/>';
+                // str += '<p class="pd-name">' + pd.pdName + '</p>';
+                // str += '<p class="pd-price">' + pd.pdPrice + ' 元</p>';
+                // str += '<p class="pd-sold">已售' + pd.pdSold + '件</p>';
+
+                var liDom = document.createElement("li");
+                liDom.innerHTML = str;
+                listDom.appendChild(liDom);
             }
         }
-        if (parseInt(inthis.y) == inthis.maxScrollY && inthis.directionY == 1) { //当滚动条的目前位置到达滚动条的最大位置的时候
-            if (end) { //如果数据加载完毕后
-                $("#scroller_down").html("没有更多数据");
-                $("#scroller_down").show();
-                if (parseInt(inthis.y) == inthis.maxScrollY) { //当滚动条归为时
-                    setTimeout(function() { //延迟加载
-                        $("#scroller_down").hide(); //up层隐藏
-                        inthis.refresh(); //页面重新加载
-                    }, 1000);
-                };
-            } else { //当数据没有加载完毕
-                var lilenght = ($("#list").find("li").length); 
-                if (lilenght == max) {
-                    end = true;
-                    $("#scroller_down").html("没有更多数据");
-                    $("#scroller_down").show();
-                    if (parseInt(inthis.y) == inthis.maxScrollY) { //当滚动条归为时
-                        setTimeout(function() { //延迟加载
-                            $("#scroller_down").hide(); //up层隐藏
-                            inthis.refresh(); //页面重新加载
-                        }, 1000);
-                    };
-                }else{
-                    $("#scroller_down").show();
-                    var flag = true;
-                    console.log(inthis.y);
-                    console.log(inthis.maxScrollY);
-                    if (parseInt(inthis.y) == inthis.maxScrollY && inthis.directionY == 1 && flag == true) { //当滚动条归为时
-                        setTimeout(function() { //延迟加载
-                            $("#scroller_down").hide(); //up层隐藏
-                            cartnewlistli ();
-                            inthis.refresh(); //页面重新加载
-                            inthis.y -= $("#list").find("li:eq(0)").height() * step;//滚动条位置定位
-                        }, 1000)
-                        flag = false;
-                    };
-                };
-            };
-        };
-        function cartnewlistli (){
-            var lilenght = ($("#list").find("li").length); //获取目前的li的数量
-            var diffnum = max - lilenght; //最大个数和总个数的差值
-            step = diffnum <= step && diffnum >= 0 ? diffnum : step; //当两个数的差值大于等于0小于等于step的时候，step等于两者之差，否则step不变
-            for (var i = 0; i < step; i++) {
-                var txtv1="";
-                    txtv1 += "<li class=\"clear mg-10 radius-10 bg-white pt-20 pb-15\">";
-                    txtv1 += " <div class=\"col-xs-3 pl-20 pr-20\">";
-                    txtv1 += "     <img src=\"..\/..\/img\/listreports\/15reportsv1.png\" class=\"full-w\" alt=\"\">";
-                    txtv1 += " <\/div>";
-                    txtv1 += " <div class=\"col-xs-9 pl-15 pr-0\">";
-                    txtv1 += "     <p class=\"f-16 col-5F5F5F f-w pt-10 pb-10\">";
-                    txtv1 += "         15分钟快速监测报告";
-                    txtv1 += "     <\/p>";
-                    txtv1 += "     <p class=\"f-16 col-5F5F5F f-w pt-10 \">";
-                    txtv1 += "         2017\/6\/20 18:34:43 <span class=\"col-FF2E24 f-14 pull-right dis-ib mr-20\">未读<\/span>";
-                    txtv1 += "     <\/p>";
-                    txtv1 += " <\/div>";
-                    txtv1 += "<\/li>";
-                $("#list").append(txtv1);
-            }
-        };
+        /*联网加载列表数据*/
+        function getListDataFromNet(pageNum, pageSize, successCallback, errorCallback) {
+            //延时一秒,模拟联网
+            setTimeout(function() {
+                //                  $.ajax({
+                //                      type: 'GET',
+                //                      url: 'xxx',
+                //                      url: 'xxx?num='+pageNum+"&size="+pageSize,
+                //                      dataType: 'json',
+                //                      success: function(data){
+                var data = pdlist1; // 模拟数据: ../res/pdlist1.js
+                //模拟分页数据
+                var listData = [];
+                for (var i = (pageNum - 1) * pageSize; i < pageNum * pageSize; i++) {
+                    if (i == data.length) break;
+                    listData.push(data[i]);
+                }
+                successCallback(listData);
+                //                      },
+                //                      error: errorCallback
+                //                  });
+            }, 500)
+        }
+
+        //禁止PC浏览器拖拽图片,避免与下拉刷新冲突;如果仅在移动端使用,可删除此代码
+        document.ondragstart = function() { return false; }
     });
 });
---
+++ js/m/pdlist1.js
@@ -0,0 +1,128 @@
+var pdlist1 = [{
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "1分钟快速监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+
+    "pdCol": "col-FF2E24",
+    "pdstate": "未读"
+}, {
+    "pdImg": "../../img/listreports/24reportsv1.png",
+    "pdName": "2小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/24reportsv1.png",
+    "pdName": "24小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-FF2E24",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "3小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "4小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "5小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-FF2E24",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "6小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "7小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "8小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-FF2E24",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "9小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "10小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "11小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "12小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "13小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-FF2E24",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "14小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "15小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "16小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "17小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "18小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "19小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}, {
+    "pdImg": "../../img/listreports/15reportsv1.png",
+    "pdName": "20小时深度监测报告",
+    "pdTime": "2017/6/20 18:34:43",
+    "pdCol": "col-E47A36",
+    "pdstate": "报告生成中"
+}];
