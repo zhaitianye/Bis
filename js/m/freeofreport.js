8ebb986e387921fb9c5a870c0cb6232b8cbf40e2
zty <1048972989@qq.com>
2017-08-31 14:11:18 +0800

    no message

--- js/m/freeofreport.js
+++ js/m/freeofreport.js
@@ -1,3 +1,45 @@
 $(document).ready(function() {
+    var ctx = document.getElementById("myChart").getContext('2d');
 
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [0.7, 0.1, 0.2],
+                backgroundColor: [
+                    'rgba(38, 186, 154, 1)',
+                    'rgba(217, 84, 79, 1)',
+                    'rgba(239, 173, 77, 1)'
+                ],
+                borderColor: [
+                    'rgba(177,224,196,1)',
+                    'rgba(177,224,196,1)',
+                    'rgba(177,224,196,1)'
+                ],
+            }],
+            labels: [
+                '正常心率',
+                '过快心率',
+                '过慢心率'
+            ]
+        },
+        options: {
+            legend: {
+                display: false,
+                labels: {
+                    boxWidth: 20,
+                },
+                position: 'right',
+            },
+            cutoutPercentage: 50,
+            layout: {
+	            padding: {
+	                left: 20,
+	                right: 15,
+	                top: 15,
+	                bottom: 15
+	            }
+	        },
+        }
+    });
 });
