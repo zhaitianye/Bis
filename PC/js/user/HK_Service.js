4aae85b027ab3be59cea311fba590cdcc2e6705b
zty <1048972989@qq.com>
2017-09-22 12:12:13 +0800

    产品激活页，未做兼容

--- PC/js/user/HK_Service.js
+++ PC/js/user/HK_Service.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
-	/*tab选项卡切换*/
+	/*激活服务部分的校验*/
+    $.validator.setDefaults({
+        submitHandler: function() {
+            alert("提交事件!");
+        }
+    });
+    $().ready(function() {
+        // 在键盘按下并释放及提交后验证提交表单
+        $(".servicact-validate").validate({
+            rules: {
+                appid: {
+                    required: true,
+                },
+                appidagain: {
+                    required: true,
+                    equalTo: ".activate-cid",
+                },
+            },
+            messages: {
+                appid: {
+                    required: "请输入悉心APP帐号",
+                },
+                appidagain: {
+                    required: "请再次输入悉心APP帐号",
+                    equalTo: "两次账号输入不一致",
+                },
+            }
+        });
+    });
+
+    /*激活部分选项框的样式*/
+    $('.activatecheckv1').iCheck({
+        checkboxClass: 'icheckbox_flat-blue',
+        radioClass: 'iradio_flat-blue'
+    });
+    $('.activatecheckv1').on('ifChecked', function(event){
+        $(".activatecheckv1").val(1);
+        var sqlcid = $(".sql-activatg-cid").val();
+        $(".activate-cid").val(sqlcid);
+        $(".activate-cidagain").val(sqlcid);
+
+    });
+    $('.activatecheckv1').on('ifUnchecked', function(event){
+        $(".activatecheckv1").val(0);
+        $(".activate-cid").val("");
+        $(".activate-cidagain").val("");
+    });
 });
+
