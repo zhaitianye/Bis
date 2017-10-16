72a98dd6abd61ee412e22b01e03ebed5155b9e2a
zty <1048972989@qq.com>
2017-10-16 15:55:46 +0800

    修改订单详情

--- PC/js/user/HK_OrderDetails.js
+++ PC/js/user/HK_OrderDetails.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     /*订单详情页*/
     /*根据状态改变样式*/
     orderstate();
-
     function orderstate() {
         var orderstatenum = Number($(".order-stateinputval").val());
         switch (orderstatenum) {
@@ -32,4 +31,20 @@ $(document).ready(function() {
                 break;
         };
     };
+    /*layer部分*/
+    layui.use('layer', function(){
+    var layer = layui.layer;
+    $(".cancellorder").click(function(){
+        layer.confirm('您确定要取消订单？', {
+              btn: ['确定','取消'] //按钮
+            }, function(){
+              layer.msg('我很伤心。', {icon: 5});
+            }, function(){
+              layer.msg('谢谢你。', {icon: 6},{
+                time: 20000, //20s后自动关闭
+                btn: ['明白了', '知道了']
+              });
+            });
+      });
+    });
 });
