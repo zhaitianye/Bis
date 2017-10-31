ef2aa0573d117dda9287eaad6b9ad552bbb387da
翟天野 <1048972989@qq.com>
2017-10-31 15:38:08 +0800

    重复订单页

--- PC/js/shopping/HK_PaySuccess.js
+++ PC/js/shopping/HK_PaySuccess.js
@@ -14,33 +14,4 @@ $(document).ready(function() {
     $(document).click(function() {
         $('.mainsearchinput').fadeOut();
     });
-    /*订单提交成功页弹出微信二维码*/
-    $(".weixinpay").click(function() {
-        $(".show-weixinpay").fadeIn();
-    });
-    $(".show-weixinpay").on("click", function(event) {
-        event.stopPropagation();
-        var target = event.target;
-        if (!$(target).closest(".weixinpay-content").length > 0 || $(target).attr("class").indexOf("close-mod") != -1) {
-            $(".show-weixinpay").fadeOut();
-        };
-    });
-    /*订单提交成功页弹出支付宝二维码*/
-    $(".zhifubaopay").click(function() {
-        $(".show-zhifubaopay").fadeIn();
-    });
-    $(".show-zhifubaopay").on("click", function(event) {
-        event.stopPropagation();
-        var target = event.target;
-        if (!$(target).closest(".zhifubaopay-content").length > 0 || $(target).attr("class").indexOf("close-mod") != -1) {
-            $(".show-zhifubaopay").fadeOut();
-        };
-    });
-    /*订单提交成功页支付方式的边框颜色改变*/
-    $(".pay-way").find("img").mouseenter(function() {
-        $(this).addClass("bor-col-309DE2");
-    });
-    $(".pay-way").find("img").mouseleave(function() {
-        $(this).removeClass("bor-col-309DE2");
-    });
 });
