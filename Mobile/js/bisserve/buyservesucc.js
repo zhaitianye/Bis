fbaf0e40f200cef23aba19e3ee66db1b728af559
翟天野 <1048972989@qq.com>
2017-11-29 15:38:42 +0800

    手机端页面

---
+++ Mobile/js/bisserve/buyservesucc.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var st_toapp = setTimeout(function(){toApp()},3000);
+    function toApp(){
+      alert("已经过了三秒了，在这里执行跳转操作。");
+     //var result={拥有的服务的}
+    }
+});
