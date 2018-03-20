0feaba16c60f37296e03ef10aed92d31db24abec
翟天野 <1048972989@qq.com>
2018-03-20 15:46:02 +0800

    库存页面

--- PC/js/admin/admin_order/HK_AdminStockControl.js
+++ PC/js/admin/admin_order/HK_AdminStockControl.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
             return false;
         });
         /*设置运单号部分的提交*/
-        form.on('submit(sub_setway)', function(data) {
+        form.on('submit(search1)', function(data) {
             /*获取此时的id*/
             var this_id = $(".ipt_waybill_id").val();
             layer.closeAll();
@@ -32,25 +32,18 @@ $(document).ready(function() {
             height: 400, //容器高度
             cols: [
                 [ //标题栏
-                    { checkbox: true, fixed: true },
-                    { field: 'id', title: 'ID', width: 80, sort: true, fixed: true },
-                    { field: 'status', title: '订单状态', width: 120, sort: true },
-                    { field: 'ordernum', title: '订单号', width: 240, event: 'ordernum_e' },
-                    { field: 'waybillnum', title: '运单号', width: 240, event: 'waybillnum_e' },
-                    { field: 'goodscontent', title: '商品内容', width: 240, event: 'goodscontent_e' },
-                    { field: 'consignee', title: '收货人', width: 160 },
-                    { field: 'phone', title: '联系电话', width: 160 },
-                    { field: 'address', title: '收货地址', width: 240 },
-                    { field: 'time', title: '下单时间', width: 180 },
-                    { field: 'logistics', title: '物流公司', width: 120, sort: true },
-                    { fixed: 'right', title: '查看详细', width: 220, align: 'center', toolbar: '#barDemo' }
+                    { field: 'sn_num', title: 'SN码', width: "25%", sort: true, fixed: true },
+                    { field: 'status', title: '产品类型', width: "25%"},
+                    { field: 'inventory_status', title: '在库状态', width: "25%", sort: true},
+                    { fixed: 'right', title: '查看详细', width: "25%", align: 'center', toolbar: '#barDemo' }
                 ]
             ],
             /*在这里使用的是静态数据，参考layui文档，使用服务器上的数据进行更替*/
+            /*sn码，产品类型，在库状态（在库，已售出），订单号，用户名，下单时间，收货人，收货电话，收货地址，保修状态*/
             data: [
-                { "id": 10000, "ordernum": "SDJHJ5418524541541", "waybillnum": "HAHGUBUN54185245415", "goodscontent": "HC3A250 悉心心电仪", "consignee": "李小姐", "phone": "18623695862", "address": "香港国宏大道305号", "time": "2017/10/12 9:55:52", "status": "待发货", "logistics": "顺丰空运", },
-                { "id": 10002, "ordernum": "SSDFF5141424541541", "waybillnum": "", "goodscontent": "电极片（*3）", "consignee": "王先森", "phone": "14851155415", "address": "深圳宝安后亭社区456号", "time": "2017/10/12 9:55:52", "status": "待配货", "logistics": "EMS", },
-                { "id": 10003, "ordernum": "GRECBEW41424541541", "waybillnum": "ETTYCCXBF418524541541", "goodscontent": "血压计(*3)", "consignee": "李先森", "phone": "16698569854", "address": "深圳罗湖区34716号", "time": "2017/10/12 9:55:52", "status": "已发货", "logistics": "顺丰空运", },
+                { "sn_num": "HC000000001", "status": "悉心心电仪 HC3A250", "inventory_status": "在库", "ordernum": "无", "username": "无", "ordertime": "无", "addressname": "无", "phone": "无",  "address": "无", "warrantystatus": "无", },
+                { "sn_num": "HC000000002", "status": "悉心心电仪 HC3A250", "inventory_status": "已售出", "ordernum": "000000000000001", "username": "youguoqiang1", "ordertime": "2018-03-19 23：15：24", "addressname": "游国强1", "phone": "13800138000",  "address": "中国 广东省 深圳市 宝安区 全至科技创新园10E", "warrantystatus": "保修中", },
+                { "sn_num": "HC000000003", "status": "悉心心电仪 HC3A250", "inventory_status": "已售出", "ordernum": "000000000000002", "username": "youguoqiang2", "ordertime": "2018-03-19 23：15：24", "addressname": "游国强2", "phone": "13800138002",  "address": "中国 广东省 深圳市 宝安区 全至科技创新园10E", "warrantystatus": "未保修", }
             ],
             page: true,
         });
@@ -60,39 +53,37 @@ $(document).ready(function() {
             var layEvent = obj.event; //获得 lay-event 对应的值
             var tr = obj.tr; //获得当前行 tr 的DOM对象
             if (layEvent === 'detail') { //查看
-                $(".detail_consignee").html(data.consignee);
-                $(".detail_phone").html(data.phone);
-                $(".detail_ordernum").html(data.ordernum);
-                $(".detail_waybillnum").html(data.waybillnum);
-                $(".detail_goodscontent").html(data.goodscontent);
-                $(".detail_address").html(data.address);
-                $(".detail_time").html(data.time);
-                $(".detail_status").html(data.status);
-                $(".detail_logistics").html(data.logistics);
-                layer.open({
-                    type: 1,
-                    title: "ID为" + data.id + "的基本信息",
-                    area: ['800px', '700px'], //宽高
-                    btn: ['我知道了'],
-                    content: $('.detailcontent'),
-                });
+                if (data.inventory_status == "已售出") {
+                    $(".detail_consignee").html(data.addressname);
+                    $(".detail_phone").html(data.phone);
+                    $(".detail_address").html(data.address);
+                    $(".detail_ordernum").html(data.ordernum);
+                    $(".detail_username").html(data.username);
+                    $(".detail_snnum").html(data.sn_num);
+                    $(".detail_goodsstatus").html(data.status);
+                    $(".detail_time").html(data.ordertime);
+                    $(".detail_warrantystatus").html(data.warrantystatus);
+                    layer.closeAll();
+                    layer.open({
+                        type: 1,
+                        title: "SN码为" + data.sn_num + "的基本信息",
+                        area: ['800px', '700px'], //宽高
+                        btn: ['我知道了'],
+                        content: $('.detailcontent'),
+                    });
+                } else{
+                    layer.msg("只有已售出的商品才能查看信息!", { icon: 5, anim: 6 });
+                }
             };
-            if (layEvent === 'edit') { //编辑
-                $(".ipt_waybill_id").val(data.id);
-                $(".ipt_waybill").val("");
-                $(".detail_waybillnum").html(data.waybillnum);
-                layer.open({
-                    type: 1,
-                    title: "编辑ID为" + data.id + "的运单信息",
-                    area: ['500px', '300px'], //宽高
-                    content: $('.detailwaybill'),
-                });
+            if (layEvent === 'del') { //编辑
+                if (data.inventory_status == "在库") {
+                    layer.msg("在这里进行删除操作!", { icon: 6});
+                } else{
+                    layer.msg("只有在库商品才能删除，已售出的不允许删除!", { icon: 5, anim: 6 });
+                }
             };
 
         });
         
     });
-    
-    
-
 });
