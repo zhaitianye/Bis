43a9c6eb379905a5183813e0328085ec42d1a314
翟天野 <1048972989@qq.com>
2018-04-09 14:57:36 +0800

    修改查看交易信息。新增退款方面的页面

--- PC/js/admin/admin_account/HK_AdminRefund.js
+++ PC/js/admin/admin_account/HK_AdminRefund.js
@@ -96,20 +96,33 @@ $(document).ready(function() {
             height: 400, //容器高度
             cols: [
                 [ //标题栏
-                    { field: 'trade_no', title: '交易号', width: 200 },
+                    { field: 'op_user_guid', title: '客服ID', width: 200 },
                     { field: 'order_guid', title: '订单号', width: 200},
                     { field: 'user_guid', title: '用户ID', width: 180 },
-                    { field: 'pay_type', title: '支付方式', width: 160, sort: true},
-                    { field: 'price', title: '交易金额', width: 160},
+                    { field: 'statu', title: '状态', width: 100, sort: true},
                     { field: 'refund_price', title: '退款金额', width: 160},
-                    { field: 'start_time', title: '交易时间', width: 200, sort: true},
-                    { field: 'status', title: '状态', width: 100, sort: true},
-
+                    { field: 'start_time', title: '创建时间', width: 200, sort: true},
+                    { field: 'refund_time', title: '退款完成时间', width: 200, sort: true},
+                    { fixed: 'right', title: '操作', width: 100, align: 'center', toolbar: '#barDemo' }
                 ]
             ],
             /*在这里使用的是静态数据，参考layui文档，使用服务器上的数据进行更替*/
-            data: [{"trade_no":"5154DEEDGRCE","user_guid":"张先生","order_guid":"SD415671235445155","pay_type":"paypay","price":"1500","refund_price":"1500","start_time":"2017/10/12 9:55:52","status":"正常"},{"trade_no":"SDERCXSDGRCE","user_guid":"王小姐","order_guid":"S1581671235445126","pay_type":"支付宝","price":"1800","refund_price":"无","start_time":"2018/01/12 19:20:52","status":"正常"},{"trade_no":"58962EEDGRCE","user_guid":"李先生","order_guid":"S4859671235445155","pay_type":"微信","price":"2600","refund_price":"无","start_time":"2012/3/3 9:55:52","status":"申请退款"},{"trade_no":"4615EEDGRCE","user_guid":"周先生","order_guid":"S488121671235445155","pay_type":"支付宝","price":"1200","refund_price":"无","start_time":"2018/03/22 15:55:52","status":"正常"},{"trade_no":"DF8591EEDGRCE","user_guid":"瑟先生","order_guid":"SD415671484198254","pay_type":"paypay","price":"1500","refund_price":"1500","start_time":"2017/10/12 9:55:52","status":"正常"}],
+            data: [{"op_user_guid":"5154DEEDGRCE","order_guid":"SD415671235445155","user_guid":"A6DE564115","statu":"未退款","refund_price":"1500","start_time":"2017/10/12 9:55:52","refund_time":"2017/10/13 9:55:52"},{"op_user_guid":"5154DEED4124","order_guid":"SD41567123544515522","user_guid":"A6DE56411522","statu":"已退款","refund_price":"2500","start_time":"2017/10/12 9:55:52","refund_time":"2017/10/13 9:55:52"},{"op_user_guid":"5154DEEDG123","order_guid":"SD415671235445155","user_guid":"A6DE564115123","statu":"未退款","refund_price":"3500","start_time":"2017/10/12 9:55:52","refund_time":"2017/10/13 9:55:52"}],
             page: true,
+        });
+        //监听工具条
+        table.on('tool(tradelist_filter)', function(obj) { //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+            var data = obj.data; //获得当前行数据
+            var layEvent = obj.event; //获得 lay-event 对应的值
+            var tr = obj.tr; //获得当前行 tr 的DOM对象
+
+            if(layEvent === 'btn_refund'){ //查看
+                layer.confirm('需要要进行  ' +data.op_user_guid +'  的退款操作么？', function(index) {
+                    /*在这里如果点确定执行的函数*/
+                    /*在这进行退款操作*/
+                    layer.close(index);
+                });
+            }
         });
 
     });
